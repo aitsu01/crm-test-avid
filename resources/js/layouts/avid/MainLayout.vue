ca798ce28eaf32b75d50661b729cf8611cdbf5af
Gianfranco Cito <citovich1975@gmail.com>
2026-04-22 10:13:40 +0200

    Implementa aggiornamento automatico delle notifiche nel layout Avid

--- resources/js/layouts/avid/MainLayout.vue
+++ resources/js/layouts/avid/MainLayout.vue
@@ -1,10 +1,32 @@
 <script setup lang="ts">
 import AvidLayout from '@/avid/layouts/MainLayout.vue';
+import { router } from '@inertiajs/vue3';
 
+
+let notificationsInterval: number | undefined;
+
+onMounted(() => {
+    notificationsInterval = window.setInterval(() => {
+        if (document.visibilityState !== 'visible') {
+            return;
+        }
+
+        router.reload({
+            preserveScroll: true,
+            preserveState: true,
+        });
+    }, 5000);
+});
+
+onUnmounted(() => {
+    if (notificationsInterval) {
+        clearInterval(notificationsInterval);
+    }
+});
 </script>
 
 <template>
     <AvidLayout>
-        <slot></slot>
+        <slot />
     </AvidLayout>
 </template>
